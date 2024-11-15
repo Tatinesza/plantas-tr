b0a0a1dc543c03d52f72ba37321abdeda3a12641
Tatinesza <mariatransito3@gmail.com>
2024-11-15 13:55:52 +0100

    mensaje script

---
+++ script.js
@@ -0,0 +1,63 @@
+//alert() muestra lo que pongamos en la consola
+/*alert("Hola mundo");
+alert(1+9+41);
+alert("1+9+41");*/
+
+/*comentario en bloque*/
+//comentario en linea//
+
+// creo variable y la relleno
+
+
+let saludo = "Holaaa";
+
+//crear variable
+let despedida;
+
+//rellenar variable
+despedida = "Adios";
+//igualar una variable a otra
+saludo = despedida;
+
+/*
+//operaciones numericas
+alert(1+2); //sumar
+alert(1-2); //restar
+alert(12/2); // dividir
+alert(8*2); //multiplicar
+alert(8%2); //resto de division
+*/
+
+let edad =16;
+edad =17;
+edad +1;
+edad++; //suma uno
+edad--; //resta uno
+
+
+//muestra lo que pongamos entre los parentesis en la consola
+console.log(edad);
+
+
+saludo = "hola";
+
+
+let nombreUsuario = "Tatinesza";
+saludo+=nombreUsuario;
+//alert(saludo+nombreUsuario); 
+//alert(saludo);
+
+alert(saludo+" "+nombreUsuario); //puedo concatenar strings usando +
+//alert(saludo);
+
+//constantes
+const SALUDO_USUARIO = saludo+nombreUsuario;
+
+
+
+
+
+
+
+
+
